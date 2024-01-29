4848e9950990081cf03b93eead8415f46950fc80
Christian Beneke <christian.beneke@traderepublic.com>
2024-01-29 22:13:07 +0100

    Fix sorting, bump to python 3.11

--- backend/src/users/router.py
+++ backend/src/users/router.py
@@ -94,11 +94,11 @@ async def router_get_user_events(
     responses = (
         db.query(DBEventResponses)
         .filter(DBEventResponses.user == user)
-        .order_by(DBEvents.start_time)
         .all()
     )
 
     # TODO this can probably be done more efficiently
     events = [response.event for response in responses if response.event]
+    events.sort(key=lambda x: x.start_time)
 
     return events
