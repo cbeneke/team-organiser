560d7f67502508160a05bb3b10e608ed196e1093
Christian Beneke <christian.beneke@traderepublic.com>
2024-11-13 12:56:44 +0100

    Add Profile view

--- app/src/screens/Login.tsx
+++ app/src/screens/Login.tsx
@@ -4,8 +4,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+// import Link from '@mui/material/Link';
+// import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -27,84 +27,84 @@ export const Login:React.FC = ()=>{
               'Content-Type': 'application/x-www-form-urlencoded',
               'Accept': 'application/json',
           },
-          body: 'grant_type=password&username=' + email + '&password=' + password + '&scope=&client_id=&client_secret='
+          body: 'grant_type=password&username=' + encodeURIComponent(email as string) + '&password=' + encodeURIComponent(password as string) + '&scope=&client_id=&client_secret='
       });
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Netzwerkantwort war nicht erfolgreich');
         }
 
         const result = await response.json();
         localStorage.setItem('access_token', result.access_token);
-        window.location.href = '/calendar'; // Redirect to calendar on successful login
+        window.location.href = '/calendar';
       } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        console.error('Es gab ein Problem bei der Anmeldung:', error);
       }
     };
     
-      return (
-        <ThemeProvider theme={defaultTheme}>
-          <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-              sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Typography component="h1" variant="h5">
-                Sign in
-              </Typography>
-              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign In
-                </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                  <Grid item>
-                    <Link href="#" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Anmelden
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="E-Mail-Adresse"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Passwort"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Angemeldet bleiben"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Anmelden
+              </Button>
+              {/* <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Passwort vergessen?
+                  </Link>
                 </Grid>
-              </Box>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Noch kein Konto? Jetzt registrieren"}
+                  </Link>
+                </Grid>
+              </Grid> */}
             </Box>
-          </Container>
-        </ThemeProvider>
-      );
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
 }
